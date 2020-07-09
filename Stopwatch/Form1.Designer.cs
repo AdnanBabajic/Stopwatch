9c8a5c67cb40a262e689ed796ea8c42672c27188
AdnanBabajic <65029737+AdnanBabajic@users.noreply.github.com>
2020-07-09 17:09:36 +0200

    First C# Windows app

---
+++ Stopwatch/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Stopwatch
+{
+    partial class Stopwatch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Stopwatch));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timerLabel = new System.Windows.Forms.Label();
+            this.start = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.reset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timerLabel
+            // 
+            this.timerLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.timerLabel.AutoSize = true;
+            this.timerLabel.BackColor = System.Drawing.Color.Purple;
+            this.timerLabel.Font = new System.Drawing.Font("Aldo the Apache", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timerLabel.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.timerLabel.Image = global::Stopwatch.Resource1.background;
+            this.timerLabel.Location = new System.Drawing.Point(71, 39);
+            this.timerLabel.Name = "timerLabel";
+            this.timerLabel.Size = new System.Drawing.Size(88, 26);
+            this.timerLabel.TabIndex = 0;
+            this.timerLabel.Text = "00:00:00";
+            this.timerLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.timerLabel.Click += new System.EventHandler(this.timerLabel_Click);
+            // 
+            // start
+            // 
+            this.start.BackgroundImage = global::Stopwatch.Resource1.button;
+            this.start.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.start.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.start.Location = new System.Drawing.Point(12, 68);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(75, 23);
+            this.start.TabIndex = 1;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // reset
+            // 
+            this.reset.BackgroundImage = global::Stopwatch.Resource1.button;
+            this.reset.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.reset.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.reset.Location = new System.Drawing.Point(145, 68);
+            this.reset.Name = "reset";
+            this.reset.Size = new System.Drawing.Size(75, 23);
+            this.reset.TabIndex = 2;
+            this.reset.Text = "Reset";
+            this.reset.UseVisualStyleBackColor = true;
+            this.reset.Click += new System.EventHandler(this.reset_Click);
+            // 
+            // Stopwatch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Stopwatch.Resource1.background;
+            this.ClientSize = new System.Drawing.Size(232, 103);
+            this.Controls.Add(this.reset);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.timerLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Stopwatch";
+            this.Text = "Stopwatch";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label timerLabel;
+        private System.Windows.Forms.Button start;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button reset;
+    }
+}
+
---
+++ Stopwatch/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stopwatch
+{
+    public partial class Stopwatch : Form
+    {
+        private int _hours, _minutes, _seconds;
+        private string sHours, sMinutes, sSeconds;
+        private Timer timer = new Timer();
+         
+        public Stopwatch()
+        {
+            InitializeComponent();
+            _hours = 0;
+            _minutes = 0;
+            _seconds = 0;
+            displayTime();
+
+            timer.Interval = (1000);
+            timer.Tick += new EventHandler(timer_Tick);
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            _seconds++;
+            if(_seconds > 60)
+            {
+                _minutes++;
+                _seconds = 0;
+            }
+
+            else if(_minutes > 60)
+            {
+                _hours++;
+                _minutes = 0;
+            }
+
+            displayTime();
+        }
+
+        private void displayTime()
+        {
+            transformIntToStringWithDoubleDigit();
+            timerLabel.Text = sHours + ":" + sMinutes + ":" + sSeconds;
+        }
+
+        private void reset_Click(object sender, EventArgs e)
+        {
+            resetTimer();
+        }
+
+        private void timerLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void transformIntToStringWithDoubleDigit()
+        {
+            sHours = string.Format("{0:00}", _hours);
+            sMinutes = string.Format("{0:00}", _minutes);
+            sSeconds = string.Format("{0:00}", _seconds);
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            start.Text = timer.Enabled ? "Start" : "Stop";
+
+            startOrStopTimer();
+        }
+
+        private void startOrStopTimer()
+        {
+            if (timer.Enabled)
+            {
+                timer.Stop();
+            }
+            else
+            {
+                timer.Start();
+            }
+        }
+
+        private void resetTimer()
+        {
+            _seconds = 0;
+            _minutes = 0;
+            _hours = 0;
+
+            displayTime();
+        }
+    }
+}
